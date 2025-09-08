d901fb878462b59f5f6f8f0c7958630f34ab4d89
yeonsoo89 <dustn9689@gmail.com>
2025-09-08 16:05:53 +0900

    실습

---
+++ 실습/반복문/level02/normal/Application3.java
@@ -0,0 +1,28 @@
+package com.yeonsoo.section03.branching.level02.normal;
+
+import java.util.Scanner;
+
+public class Application3 {
+    public static void main(String[] args) {
+        /* 정수를 입력받아 1부터 입력받은 정수까지
+         * 홀수이면 "수", 짝수이면 "박"이 정수만큼 누적되어 출력되게 작성하시오.
+         *
+         * -- 입력 예시 --
+         * 정수를 입력하세요 : 5
+         *
+         * -- 출력 예시 --
+         * 수박수박수
+         * */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("정수를 입력하세요 : ");
+        int num = sc.nextInt();
+
+        for(int i = 0; i < num; i++) {
+            if (i % 2 == 0) {
+                System.out.print("박");
+            } else {
+                System.out.print("수");
+            }
+        }
+    }
+}
